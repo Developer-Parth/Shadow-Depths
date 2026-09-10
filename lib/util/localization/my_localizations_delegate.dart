87d376ba0157099ed666d7181490e348a752aa27
Developer-Parth <166241612+Developer-Parth@users.noreply.github.com>
2026-09-10 14:46:54 +0530

    Support all 127 locales in delegate with proper resolution

--- lib/util/localization/my_localizations_delegate.dart
+++ lib/util/localization/my_localizations_delegate.dart
@@ -1,19 +1,24 @@
 import 'dart:async';
 
 import 'package:shadow_depths/util/localization/my_localizations.dart';
+import 'package:shadow_depths/util/localization/languages.dart';
 import 'package:flutter/material.dart';
 
 class MyLocalizationsDelegate extends LocalizationsDelegate<MyLocalizations> {
   const MyLocalizationsDelegate();
 
   @override
-  bool isSupported(Locale locale) => ['en', 'pt'].contains(locale.languageCode);
+  bool isSupported(Locale locale) {
+    return languageOrder.any((code) {
+      String langCode = code.contains('-') ? code.split('-')[0] : code;
+      return langCode == locale.languageCode;
+    });
+  }
 
   @override
   Future<MyLocalizations> load(Locale locale) async {
     MyLocalizations localizations = MyLocalizations(locale);
     await localizations.load();
-    print("Load ${locale.languageCode}");
     return localizations;
   }
 
@@ -21,18 +26,26 @@ class MyLocalizationsDelegate extends LocalizationsDelegate<MyLocalizations> {
   bool shouldReload(MyLocalizationsDelegate old) => false;
 
   Locale resolution(Locale? locale, Iterable<Locale> supportedLocales) {
-    for (Locale supportedLocale in supportedLocales) {
-      if (locale != null) {
-        if (supportedLocale.languageCode == locale.languageCode ||
-            supportedLocale.countryCode == locale.countryCode) {
-          return supportedLocale;
+    if (locale == null) return supportedLocales.first;
+
+    List<Locale> languageMatches = supportedLocales
+        .where((l) => l.languageCode == locale.languageCode)
+        .toList();
+
+    if (languageMatches.isEmpty) return supportedLocales.first;
+
+    if (locale.countryCode != null) {
+      for (Locale match in languageMatches) {
+        if (match.countryCode == locale.countryCode) {
+          return match;
         }
       }
     }
-    return supportedLocales.first;
+
+    return languageMatches.first;
   }
 
   static List<Locale> supportedLocales() {
-    return [const Locale('en', 'US'), const Locale('pt', 'BR')];
+    return languageOrder.map((code) => codeToLocale(code)).toList();
   }
 }
